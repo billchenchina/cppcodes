eab90b3267d18b1354ef659cb63c8d0291e6f52e
陈天宇 <billchenchina@users.noreply.github.com>
2017-05-23 00:39:38 -0500

    2439.cpp Unfinished

---
+++ OpenJudge/2439.cpp
@@ -0,0 +1,57 @@
+//Unfinished
+#include <iostream>
+#define mix(x,y) x+(y-x)/2
+using namespace std;
+struct cowRange {
+    int leftRange;
+    int rightRange;
+    int maxInRange;
+    int minInRange;
+    cowRange *lchild,*rchild;
+    cowRange(int leftR,int rightR) {
+        this->leftRange=leftR;
+        this->rightRange=rightR;
+        this->maxInRange=0;
+        this->minInRange=INT_MAX;
+        if(leftR==rightR) {
+            this->lchild=this->rchild=NULL;
+            return;
+        } else {
+            int middle=mix(leftR,rightR);
+            lchild=new cowRange(leftR,middle);
+            rchild=new cowRange(middle+1,rightR);
+        }
+    }
+    void addNum(int position,int value) {
+        this->maxInRange=max(this->maxInRange,value);
+        this->minInRange=min(this->minInRange,value);
+        if(this->leftRange==this->rightRange&&position==this->leftRange) {
+            return;
+        } else if(mix(this->rightRange,this->leftRange)>=position) {
+            lchild->addNum(position,value);
+        } else if(mix(this->rightRange,this->leftRange)<position) {
+            rchild->addNum(position,value);
+        }
+    }
+    int searchValueInRange(int leftR,int RightR){
+
+    }
+};
+
+int main() {
+    int N,Q;
+    cin>>N>>Q;
+    cowRange *root=new cowRange(1,N);
+    for(int i=1; i<=N; i++) {
+        int tmp;
+        cin>>tmp;
+        root->addNum(i,tmp);
+    }
+    for(int i=1; i<=Q; i++) {
+        int l,r;
+        cin>>l>>r;
+
+    }
+
+}
+
