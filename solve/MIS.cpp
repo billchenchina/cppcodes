94bf11f2c82a24a90c27c1d4ae0252aca2318429
陈天宇 <billchenchina@users.noreply.github.com>
2017-06-16 20:28:12 +0800

    create MIS.cpp

---
+++ solve/MIS.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+	vector<int>v;
+	int m,sum;
+	int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int tmp;
+        cin>>tmp;
+        v.push_back(tmp);
+    }
+    m=v[0];
+    sum=v[0];
+	for(int i=1;i<v.size();i++){
+	    sum=max(sum,0);
+	    sum+=v[i];
+	    m=max(m,sum);
+	}
+    cout<<m;
+}
