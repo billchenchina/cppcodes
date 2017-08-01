841d98e40868ebdc530b85593221b913a4e4be98
billchenchina <billchenchina2001@gmial.com>
2017-08-01 20:54:42 +0800

    update

---
+++ SPJ/checker_of_家谱树.cpp
@@ -0,0 +1,40 @@
+#include "testlib.h"
+#include <iostream>
+#include <vector>
+
+// checker of "¼ÒÆ×Ê÷"
+int main(int argc,char *argv[])
+{
+    setName("Test the answer");
+    registerTestlibCmd(argc, argv);
+    bool correct=1;
+    int N;
+    N=inf.readInt();
+    std::vector<int>pos(N+1,0);
+    for(int i=1; i<=N; i++) {
+        int a;
+        a=ouf.readInt();
+        pos[a]=i;
+    }
+    for(int i=1; i<=N; i++) {
+        while(true) {
+            int a;
+            a=inf.readInt();
+            if(a!=0) {
+                if(pos[i]>pos[a])
+                    correct=0;
+            } else break;
+        }
+    }
+
+
+    if(correct) {
+        quitf(_ok, "The answer is correct.");
+    } else {
+        quitf(_wa, "The answer is wrong");
+    }
+}
+
+
+
+
