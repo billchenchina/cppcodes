d81e7cfeb078d2bc4dbe07e9087500bba75b14f3
billchenchina <billchenchina2001@gmail.com>
2017-08-04 13:57:34 +0800

    update

--- SPJ/checker_of_家谱树.cpp
+++ SPJ/checker_of_家谱树.cpp
@@ -1,6 +1,7 @@
 #include "testlib.h"
 #include <iostream>
 #include <vector>
+#include <set>
 
 int main(int argc,char *argv[])
 {
@@ -10,15 +11,19 @@ int main(int argc,char *argv[])
     int N;
     N=inf.readInt();
     std::vector<int>pos(N+1,0);
+    std::set<int>answer_set;
     for(int i=1; i<=N; i++) {
         int a;
         a=ouf.readInt();
         pos[a]=i;
+        answer_set.insert(i);
     }
+    std::set<int>output_set;
     for(int i=1; i<=N; i++) {
         while(true) {
             int a;
             a=inf.readInt();
+            output_set.insert(a);
             if(a!=0) {
                 if(pos[i]>pos[a])
                     correct=0;
