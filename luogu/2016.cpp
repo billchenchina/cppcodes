f4f17c849596dc1deede1cb073c538e85dae8a2a
Billchenchina <billchenchina2001@gmail.com>
2017-08-29 22:32:40 +0800

    update

--- luogu/2016.cpp
+++ luogu/2016.cpp
@@ -1,1 +1,22 @@
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+    scanf("%d",&N);
+    for(int i=0;i<N;++i)
+    {
+        int father,k;
+        scanf("%d%d",&father,&k);
+        for(int i=0;i<k;++i)
+        {
+            int son;
+            scanf("%d",&son);
+            addedge(father,son);
+        }
+    }
+}
