0f04788910bd01f80ac6f0695d0a69c74f4ebc2e
Billchenchina <billchenchina2001@gmail.com>
2017-09-02 07:24:32 +0800

    update

--- luogu/1880.cpp
+++ luogu/1880.cpp
@@ -5,46 +5,51 @@
 
 using namespace std;
 
-
-
+vector<vector<int> >dp_map1(105,vector<int>(105+1,0));
+vector<int>prefix_amount(2*105);
 int main()
 {
     ios::sync_with_stdio(false);
     int N;
     cin>>N;
-    vector<vector<int> >dp_map1(N,vector<int>(N+1,-1));
-    vector<int>amount(2*N);
-    for(int i=0;i<N;++i)
+
+    for(int i=0; i<N; ++i)
     {
-        cin>>amount[i];
-        amount[i+N]=amount[i];
+        int tmp;
+        cin>>tmp;
+        prefix_amount[i]=prefix_amount[i-1]+tmp;
     }
-    int minn=INT_MAX;
+    for(int i=N;i<2*N;++i)
+    {
+        prefix_amount[i]=prefix_amount[i-1]+prefix_amount[i-N]-prefix_amount[i-N-1];
+    }
     {
         int ttlrange=2;
-        for(int j=0;j<N;++j)
+        for(int j=0; j<N; ++j)
         {
-            dp_map1[j][ttlrange]=amount[j]+amount[j+1];
+            dp_map1[j][ttlrange]=prefix_amount[j+1]-prefix_amount[j-1];
         }
     }
 
-    for(int ttlrange=3;ttlrange<=N;++ttlrange)
+    for(int ttlrange=3; ttlrange<=N; ++ttlrange)
     {
-        for(int start=0;start<N;++start)
+        for(int start=0; start<N; ++start)
         {
             int minn=INT_MAX;
-            for(int firstrange=0;firstrange<=ttlrange;++firstrange)
+            for(int firstrange=1; firstrange<=ttlrange; ++firstrange)
             {
                 if(minn>dp_map1[start][firstrange]+dp_map1[start+firstrange][ttlrange-firstrange])
                 {
                     minn=dp_map1[start][firstrange]+dp_map1[start+firstrange][ttlrange-firstrange];
-
                 }
             }
-            dp_map1[start][ttlrange]=minn;
+            dp_map1[start][ttlrange]=minn+prefix_amount[start+ttlrange]-prefix_amount[start-1];
         }
     }
-
-    cout<<dp_map1[0][N];
+    int minn=INT_MAX;
+    for(int i=0;i<N;++i)
+    {
+        cout<<dp_map1[i][N]<<' ';
+    }
 }
 
