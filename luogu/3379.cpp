cf521c761e80083eb9a9111171f618d1abcf4e47
billchenchina <billchenchina2001@gmail.com>
2017-09-04 21:14:09 +0800

    update LCA => RMQ
    luogu/3379.cpp

---
+++ luogu/3379.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+int N,M,root;
+vector<int>mapn[500001];
+
+vector<int>dfs_list;
+
+int dfn[500001];
+bool vis[500001];
+int first_pos[500001];
+int last_pos[500001];
+int defaultdfn=1;
+int _M[500001][21];
+int another_dfn[500001];
+void process();
+void process2();
+int getmin(int a,int b);
+void connect();
+void dfs(int i);
+
+
+
+int main()
+{
+    cin>>N>>M>>root;
+    for(int i=0; i<N-1; ++i)
+    {
+        connect();
+    }
+    dfs(root);
+
+    process();
+    for(int i=0; i<M; ++i)
+    {
+        process2();
+    }
+}
+
+
+
+void connect()
+{
+    int x,y;
+    cin>>x>>y;
+    mapn[x].push_back(y);
+    mapn[y].push_back(x);
+}
+
+
+
+void dfs(int i)
+{
+    vis[i]=1;
+    int vsize=mapn[i].size();
+    dfn[i]=defaultdfn++;
+    another_dfn[dfn[i]]=i;
+    dfs_list.push_back(dfn[i]);
+    first_pos[dfn[i]]=dfs_list.size()-1;
+    for(int j=0; j<vsize; ++j)
+    {
+        int nextp=mapn[i][j];
+        if(!vis[nextp])
+        {
+            dfs(nextp);
+            dfs_list.push_back(dfn[i]);
+        }
+    }
+    last_pos[dfn[i]]=dfs_list.size()-1;
+}
+
+void process2()
+{
+    int x,y;
+    cin>>x>>y;
+    x=dfn[x];
+    y=dfn[y];
+    if(x>y)
+    {
+        swap(x,y);
+    }
+    x=first_pos[x];y=last_pos[y];
+
+    cout<<another_dfn[getmin(x,y)]<<endl;
+}
+
+int getmin(int a,int b)
+{
+    int k=log2(b-a+1);
+    if((b-a+1)&(b-a))
+    {
+        int min1=dfs_list[_M[a][k]];
+        int min2=dfs_list[_M[b-(1<<(k))][k]];
+        return min1<min2?min1:min2;
+    }
+    else
+    {
+        return dfs_list[_M[a][k]];
+    }
+}
+
+void process() {
+    vector<int>&A=dfs_list;
+    int N=A.size();
+    int i, j;
+    for (i = 0; i < N; i++)
+        _M[i][0] = i;
+    for (j = 1; 1 << j <= N; j++)
+        for (i = 0; i + (1 << j) - 1 < N; i++)
+            if (A[_M[i][j - 1]] < A[_M[i + (1 << (j - 1))][j - 1]])
+                _M[i][j] = _M[i][j - 1];
+            else
+                _M[i][j] = _M[i + (1 << (j - 1))][j - 1];
+}
+
+
+
