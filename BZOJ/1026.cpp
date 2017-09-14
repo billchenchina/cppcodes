b1bc031b306671f673d696ae568a8ac53aa8421e
billchenchina <billchenchina2001@gmail.com>
2017-09-14 16:12:00 +0800

    update

---
+++ BZOJ/1026.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int solve(int num)
+{
+
+}
+
+int main()
+{
+  int A,B;
+  scanf("%d%d",&A,&B);
+  printf("%d",solve(B)-solve(A-1));
+}
