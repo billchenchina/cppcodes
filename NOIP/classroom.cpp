f68c62928fdb57cffd54622ac7fc03db9c60860c
billchenchina <billchenchina2001@gmail.com>
2017-10-26 16:18:11 +0800

    update

---
+++ NOIP/classroom.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,m,v,e;
+    cin>>n>>m>>v>>e;
+    int c[2000];
+    int d[2000];
+    double k[2000];
+    for(int i=0;i<n;++i)
+    {
+        cin>>c[i];
+    }
+    for(int i=0;i<n;++i)
+    {
+        cin>>d[i];
+    }
+    for(int i=0;i<n;++i)
+    {
+        cin>>k[i];
+    }
+    for(int i=0;i<e;++i)
+    {
+
+    }
+}
