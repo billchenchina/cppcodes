b9a611f9683d60dcc14a501eebbec6897a98d772
billchenchina <billchenchina2001@gmail.com>
2017-10-26 21:39:32 +0800

    update unfinished classroom.cpp

--- NOIP/classroom.cpp
+++ NOIP/classroom.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+struct edge
+{
+    int to;
+    int weight;
+};
+
+vector<vector<int> >mapn(300,vector<int>(300,INT_MAX));
+
 int main()
 {
     int n,m,v,e;
@@ -23,6 +31,23 @@ int main()
     }
     for(int i=0;i<e;++i)
     {
-
+        int a,b,w;
+        cin>>a>>b>>w;
+        mapn[a][b]=w;
+        mapn[b][a]=w;
     }
+    for(int i=0;i<v;++i)
+    {
+        for(int j=0;j<v;++j)
+        {
+            for(int k=0;k<v;++k)
+            {
+                if(mapn[i][j]!=INT_MAX&&mapn[i][k]!=INT_MAX&&mapn[j][k]<mapn[j][i]+mapn[i][k])
+                {
+                    mapn[j][k]=mapn[k][j]=mapn[j][i]+mapn[i][k];
+                }
+            }
+        }
+    }
+
 }
