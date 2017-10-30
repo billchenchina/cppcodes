8f1510c2e4bf8dcd7423cd1284f60e43a0e1273c
billchenchina <billchenchina2001@gmail.com>
2017-10-30 14:35:24 +0800

    update :joy:

--- BZOJ/1036.cpp
+++ BZOJ/1036.cpp
@@ -2,57 +2,83 @@
 
 using namespace std;
 
-struct edge
+struct Edge
 {
-    int next;
     int to;
+    Edge *next;
 };
 
-edge edges[30001];
-int first_l[30001];
-int weight[30001];
-void add_edge(int u,int v,int i)
+Edge *edges[30001];
+
+// 加边
+void connect(int a,int b)
 {
-    edges[i].next=first_l[u];
-    edges[i].to=v;
-    first_l[u]=i;
+    Edge *now=new Edge;
+    now->to=b;
+    now->next=edges[a];
+    edges[a]=now;
 }
 
+void read(int&b)
+{
+    char a;
+    int res=0,flag=1;
+    a=getchar();
+    while(!isdigit(a))
+    {
+        if(a=='-')
+        {
+            flag=-1;
+        }
+        a=getchar();
+    }
+    while(isdigit(a))
+    {
+        res=res*10+a-'0';
+        a=getchar();
+    }
+    b=res;
+}
+
+int n;
+
+#define mid ((l_r+r_r)>>1)
+
+struct treeNode
+{
+    int l_r;
+    int r_r;
+    treeNode *l_c;
+    treeNode *r_c;
+    treeNode(int l,int r)
+    {
+        if(l==r)
+        {
+            l_r=l;
+            r_r=r;
+            l_c=r_c=NULL;
+        }
+        else
+        {
+            l_r=l;
+            r_r=r;
+            l_c=new treeNode(l,mid);
+            r_c=new treeNode(mid+1,r);
+        }
+    }
+    void
+};
+#undef mid
 
 int main()
 {
-    int n;
-    scanf("%d",&n);
-    for(int i=0;i<n-1;++i)
+    read(n);
+    for(int i=0; i<n-1; ++i)
     {
-        int u,v;
-        scanf("%d%d",&u,&v);
-        add_edge(u,v,i);
+        int a,b;
+        read(a);
+        read(b);
+        connect(a,b);
     }
-    for(int i=1;i<=n;++i)
-    {
-        scanf("%d",&weight[i]);
-    }
-    int Q;
-    scanf("%d",&Q);
-    while(Q--)
-    {
-        char index[10];
-        scanf("%s",index);
-        if(strcmp(index,"QMAX")==0)
-        {
-            int u,v;
-            scanf("%d%d",&u,&v);
-        }
-        else if(strcmp(index,"CHANGE")==0)
-        {
-            int u,v;
-            scanf("%d%d",&u,&v);
-        }
-        else if(strcmp(index,"QSUM")==0)
-        {
-            int u,v;
-            scanf("%d%d",&u,&v);
-        }
-    }
+
 }
