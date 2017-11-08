b2db1bdb985117f285a1da7b756c1ce7a422ee91
Billchenchina <billchenchina2001@gmail.com>
2017-11-08 22:23:47 +0800

    revert 100.cpp and add 100_2.cpp

--- HLSSYOJ/100.cpp
+++ HLSSYOJ/100.cpp
@@ -3,39 +3,19 @@
 using namespace std;
 int main()
 {
-    ios::sync_with_stdio(false);
-     cin.tie(0);
     int n;
     cin>>n;
-    vector<int>vbs_(n);
-    vector<bitset<32> >vbs;
-    for(int i=0; i<n; ++i)
+    vector<bitset<32> >vbs(n);
+    for(int i=0;i<n;++i)
     {
-        cin>>vbs_[i];
+        int x;
+        cin>>x;
+        vbs[i]=x;
     }
-    sort(vbs_.begin(),vbs_.end());
-    for(int i=0; i<n; ++i)
-    {
-        if(i<=1)
-        {
-            vbs.push_back(vbs_[i]);
-        }
-        else
-        {
-            if(vbs_[i]==vbs_[i-1]&&vbs_[i-1]==vbs_[i-2])
-            {
-                continue;
-            }
-            else
-            {
-                vbs.push_back(vbs_[i]);
-            }
-        }
-    }
-    for(int j=31; j>=0; --j)
+    for(int j=31;j>=0;--j)
     {
         vector<bitset<32> >b;
-        for(int i=0; i<vbs.size(); ++i)
+        for(int i=0;i<vbs.size();++i)
         {
             if(vbs[i][j])
             {
@@ -53,14 +33,14 @@ int main()
         }
     }
     vector<int>ans_vec;
-    for(int i=0; i<vbs.size(); ++i)
+    for(int i=0;i<vbs.size();++i)
     {
         ans_vec.push_back(vbs[i].to_ulong());
     }
     int maxn=-1;
-    for(int i=0; i<ans_vec.size(); ++i)
+    for(int i=0;i<ans_vec.size();++i)
     {
-        for(int j=0; j<i; ++j)
+        for(int j=0;j<i;++j)
         {
             if((ans_vec[i]&ans_vec[j])>maxn)
             {
