d9208de7e5293395b257a162652fa3b9c53268cf
billchenchina <billchenchina2001@gmail.com>
2017-11-08 10:18:40 +0800

    update HLSSYOJ/100.cpp

--- HLSSYOJ/100.cpp
+++ HLSSYOJ/100.cpp
@@ -1,60 +1,52 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-string to_binary_string(int x)
-{
-    string str="";
-    while(x)
-    {
-        str.append(1,'0'+x%2);
-        x>>=1;
-    }
-    reverse(str.begin(),str.end());
-    return str;
-}
-
 int main()
 {
     int n;
     cin>>n;
-    vector<bitset<32> >bst(n);
-    vector<int>selected[32];
-    int maxn=0;
+    vector<bitset<32> >vbs(n);
     for(int i=0;i<n;++i)
     {
-        unsigned int x;
+        int x;
         cin>>x;
-        bst[i]=x;
-        for(int j=0;j<32;++j)
+        vbs[i]=x;
+    }
+    for(int j=31;j>=0;--j)
+    {
+        vector<bitset<32> >b;
+        for(int i=0;i<vbs.size();++i)
         {
-            if(bst[i][j])
+            if(vbs[i][j])
             {
-                selected[j].push_back(x);
+                b.push_back(vbs[i]);
             }
         }
-    }
-    vector<int>vec;
-    for(int i=31;i>=0;--i)
-    {
-        if(selected[i].size()>=2)
+        if(b.size()>=2)
         {
-            vec=selected[i];
-            break;
+            vbs=b;
+            b.clear();
+        }
+        else
+        {
+            continue;
         }
     }
-    for(int i=0;i<vec.size();++i)
+    vector<int>ans_vec;
+    for(int i=0;i<vbs.size();++i)
+    {
+        ans_vec.push_back(vbs[i].to_ulong());
+    }
+    int maxn=-1;
+    for(int i=0;i<ans_vec.size();++i)
     {
         for(int j=0;j<i;++j)
         {
-            if((vec[i]&vec[j])>maxn)
+            if((ans_vec[i]&ans_vec[j])>maxn)
             {
-                maxn=vec[i]&vec[j];
+                maxn=ans_vec[i]&ans_vec[j];
             }
         }
     }
     cout<<maxn;
-
-
-
 }
