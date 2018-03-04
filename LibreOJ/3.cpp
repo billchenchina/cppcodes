25c04376f82e48806efde29895cd061a85bd84a3
Billchenchina <billchenchina2001@gmail.com>
2018-03-04 13:50:33 +0800

    Create 3.cpp

---
+++ LibreOJ/3.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ifstream fin("copycat.in");
+	ofstream fout("copycat.out");
+	#define cin fin
+	#define cout fout
+	int T;
+	cin>>T;
+	string str;
+	while(cin>>str)
+	{
+		cout<<str<<endl;
+	}
+	
+}
