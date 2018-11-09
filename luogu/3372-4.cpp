f8cac1b50c36fc07ac6ad2bdf9dbe69fb3f17003
Billchenchina <billchenchina2001@gmail.com>
2018-11-09 21:48:27 +0800

    Create 3372-4.cpp

---
+++ luogu/3372-4.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool verbose_mode = false;
+
+struct SegTree
+{
+    SegTree *left_sub_tree;
+    SegTree *right_sub_tree;
+    int left_range;
+    int right_range;
+    int sum;
+    SegTree(int _left_range, int _right_range):left_range(_left_range), right_range(_right_range)
+    {
+        sum = 0;
+        if(verbose_mode)
+        {
+            cout << "SegTree(" << _left_range
+                 << ", " << _right_range << ")" << endl;
+        }
+        if(_left_range == _right_range)
+        {
+            left_sub_tree = right_sub_tree = NULL;
+        }
+        else
+        {
+#define mid _left_range + ((_right_range - _left_range)>>1)
+            left_sub_tree = new SegTree(left_range, mid);
+            right_sub_tree = new SegTree(mid+1, right_range);
+#undef mid
+        }
+    }
+    void add(int _left_range, int _right_range, int value)
+    {
+        if(verbose_mode)
+        {
+            cout << "SegTree::add(" << _left_range
+                 << ", " << _right_range
+                 << ", " << value << ")" << endl;
+            cout << "Current Node status:" << endl
+                 << left_range << ", " << right_range << endl;
+        }
+        sum += 1ll*(_right_range - _left_range +1)*value;
+        if(_left_range == _right_range)
+        {
+            return;
+        }
+        if(_left_range > left_range + ((right_range - left_range)>>1))
+        {
+            right_sub_tree->add(_left_range, _right_range, value);
+        }
+        else if(_right_range <= left_range + ((right_range - left_range)>>1))
+        {
+            left_sub_tree->add(_left_range, _right_range, value);
+        }
+        else
+        {
+            left_sub_tree->add(_left_range, left_range + ((right_range - left_range)>>1), value);
+            right_sub_tree->add(left_range + ((right_range - left_range)>>1)+1, _right_range, value);
+        }
+    }
+    long long query(int _left_range, int _right_range)
+    {
+        if(verbose_mode)
+        {
+            cout << "SegTree::query(" << _left_range
+                 << ", " << _right_range << ")" << endl;
+            cout << "Current Node status:" << endl
+                 << left_range << ", " << right_range << endl;
+
+        }
+        if(_left_range == left_range && _right_range == right_range)
+        {
+            return sum;
+        }
+        if(_left_range > left_range + ((right_range - left_range)>>1))
+        {
+            return right_sub_tree -> query(_left_range, _right_range);
+        }
+        if(_right_range <= left_range + ((right_range - left_range)>>1))
+        {
+            return left_sub_tree -> query(_left_range, _right_range);
+        }
+        return left_sub_tree -> query(_left_range, left_range + ((right_range - left_range)>>1))
+               + right_sub_tree -> query(left_range + ((right_range - left_range)>>1)+1, _right_range);;
+    }
+};
+
+
+int main(int argc, char **argv)
+{
+    if(argc == 2 && strcmp(argv[1], "verbose") == 0)
+    {
+        verbose_mode = true;
+        cout << "verbose mode on" << endl;
+    }
+    int N, M;
+    cin >> N >> M;
+    SegTree *root = new SegTree(1, N);
+    for(int i = 1; i <= N; ++i)
+    {
+        int x;
+        cin >> x;
+        root->add(i, i, x);
+    }
+    for(int i = 0; i < M; ++i)
+    {
+        if(verbose_mode) {
+            cout << "Waiting user input" << endl;
+        }
+        int op;
+        cin >> op;
+        if(op == 1)
+        {
+            int x, y, k;
+            cin >> x >> y >> k;
+            root->add(x, y, k);
+        }
+        else if(op == 2)
+        {
+            int x, y;
+            cin >> x >> y;
+            cout << root->query(x, y) << endl;
+        }
+    }
+}
